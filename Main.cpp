bde7da6f9ae93b844446648760f153c17f8dff64
GenaBitu <genabitu@gmail.com>
2014-08-23 22:33:31 +0200

    Main - brace-init

--- Main.cpp
+++ Main.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 using namespace glm;
 
-std::ofstream ERROR("ErrorLog.txt", fstream::trunc);
+std::ofstream ERROR{"ErrorLog.txt", fstream::trunc};
 GLFWwindow* WINDOW;
-Camera* MAINCAM = new Camera(45, 4.0 / 3.0, 0.1, vec3(0, 0, -10), quat(0.9238795325112867, -0.3826834323650897, 0, 0));
+Camera* MAINCAM = new Camera{45, 4.0 / 3.0, 0.1, vec3{0, 0, -10}, quat{0.9238795325112867, -0.3826834323650897, 0, 0}};
 double DELTA;
-float SPEED = 1.0;
-int PAUSE = 0;
+float SPEED {1.0};
+int PAUSE {0};
 double XCURSOR;
 double YCURSOR;
 int WIDTH;
@@ -25,7 +25,7 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    GLFWmonitor* monitor = glfwGetPrimaryMonitor();
+    GLFWmonitor* monitor {glfwGetPrimaryMonitor()};
     WINDOW = glfwCreateWindow(1400, 1050, "OpenStrategia", monitor, nullptr);
     glfwMakeContextCurrent(WINDOW);
     if(!WINDOW)
@@ -40,7 +40,7 @@ int main()
     glfwGetWindowSize(WINDOW, &WIDTH, &HEIGHT);
 
     glewExperimental = GL_TRUE;
-    GLenum GLEWerror = glewInit();
+    GLenum GLEWerror {glewInit()};
     if(GLEWerror != GLEW_OK)
     {
         ERROR << "Failed to initialize GLEW: " << glewGetErrorString(GLEWerror) << endl;
@@ -53,7 +53,7 @@ int main()
     glDepthFunc(GL_LESS);
 
     // Loading shaders
-    Program* shaders = new Program;
+    Program* shaders {new Program};
     shaders->AddShader("phong.vertex.glsl", GL_VERTEX_SHADER);
     shaders->AddShader("phong.fragment.glsl", GL_FRAGMENT_SHADER);
     if(glfwWindowShouldClose(WINDOW)) {return -1;}
@@ -67,7 +67,7 @@ int main()
     glfwSwapBuffers(WINDOW);
     glfwPollEvents();
 
-                                                                        vector<GLfloat> vData = {
+                                                                        vector<GLfloat> vData {
 		-1.0f,-1.0f,-1.0f,
 		-1.0f,-1.0f, 1.0f,
 		-1.0f, 1.0f,-1.0f,
@@ -77,17 +77,16 @@ int main()
 		 1.0f,-1.0f,-1.0f,
 		 1.0f, 1.0f, 1.0f,
 	};
-                                                                        vector<GLfloat> vData2 = {
+                                                                        vector<GLfloat> vData2 {
 		-0.5f,-1.0f, 0.0f,
 		 1.0f,-1.0f, 0.0f,
 		 1.0f, 1.0f, 0.0f,
 	};
-                                                                        vector<GLuint> iData = {0,1,2,1,3,2,4,7,5,6,4,5,1,5,7,1,7,3,2,4,0,6,0,4,3,7,4,3,4,2,1,5,6,1,6,0};
-                                                                        vector<GLuint> iData2 = {0,1,2};
-                                                                        //RenderObject3D* objekt = new RenderObject3D(&vData, &iData);
-                                                                        //RenderObject2D* objekt2 = new RenderObject2D(&vData2, &iData2);
-                                                                        RenderObject3D* objekt3 = new RenderObject3D("tank.obj");
-                                                                        //Texture* tex1 = new Texture("tank-tex.bmp");
+                                                                        vector<GLuint> iData {0,1,2,1,3,2,4,7,5,6,4,5,1,5,7,1,7,3,2,4,0,6,0,4,3,7,4,3,4,2,1,5,6,1,6,0};
+                                                                        vector<GLuint> iData2 {0,1,2};
+                                                                        //RenderObject3D* objekt {new RenderObject3D(&vData, &iData)};
+                                                                        //RenderObject2D* objekt2 {new RenderObject2D(&vData2, &iData2)};
+                                                                        RenderObject3D* objekt3 {new RenderObject3D("tank.obj")};
     while(!glfwWindowShouldClose(WINDOW)) // Main loop
     {
         // Rendering
