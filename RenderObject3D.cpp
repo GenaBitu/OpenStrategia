d74c0657bbcb2d8b5cecf836839d5815c0806bae
GenaBitu <genabitu@gmail.com>
2014-08-25 16:17:24 +0200

    RenderObject3D - brace-init

--- RenderObject3D.cpp
+++ RenderObject3D.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 using namespace glm;
 
-RenderObject3D::RenderObject3D() : NBO(0)                   //DEBUG ONLY
+RenderObject3D::RenderObject3D() : NBO{}                   //DEBUG ONLY
 {
     glGenBuffers(1, &NBO);
-    const GLfloat vertex_buffer_data[] = {
+    const GLfloat vertex_buffer_data[]{
 		-1.0f,-1.0f,-1.0f,
 		-1.0f,-1.0f, 1.0f,
 		-1.0f, 1.0f,-1.0f,
@@ -15,7 +15,7 @@ RenderObject3D::RenderObject3D() : NBO(0)                   //DEBUG ONLY
 		 1.0f,-1.0f,-1.0f,
 		 1.0f, 1.0f, 1.0f,
 	};
-	const GLuint element_buffer_data[] = {0,1,2,1,3,2,4,7,5,6,4,5,1,5,7,1,7,3,2,4,0,6,0,4,3,7,4,3,4,2,1,5,6,1,6,0};
+	const GLuint element_buffer_data[]{0,1,2,1,3,2,4,7,5,6,4,5,1,5,7,1,7,3,2,4,0,6,0,4,3,7,4,3,4,2,1,5,6,1,6,0};
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_buffer_data), vertex_buffer_data, GL_STATIC_DRAW);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
@@ -23,9 +23,9 @@ RenderObject3D::RenderObject3D() : NBO(0)                   //DEBUG ONLY
 	elementCount = 36;
 }
 
-RenderObject3D::RenderObject3D(const RenderObject3D& other) : RenderObject(other), NBO(0)
+RenderObject3D::RenderObject3D(const RenderObject3D& other) : RenderObject(other), NBO{}
 {
-    GLint bufferSize = 0;
+    GLint bufferSize{};
     glGenBuffers(1, &NBO);
     glBindBuffer(GL_COPY_READ_BUFFER, other.NBO);
     glGetBufferParameteriv (GL_COPY_READ_BUFFER, GL_BUFFER_SIZE, &bufferSize);
@@ -40,7 +40,7 @@ RenderObject3D::RenderObject3D(const RenderObject3D& other) : RenderObject(other
 RenderObject3D& RenderObject3D::operator=(const RenderObject3D& other)
 {
     RenderObject::operator=(other);
-    GLint bufferSize = 0;
+    GLint bufferSize{};
     glBindBuffer(GL_COPY_READ_BUFFER, other.NBO);
     glGetBufferParameteriv (GL_COPY_READ_BUFFER, GL_BUFFER_SIZE, &bufferSize);
     if(bufferSize > 0)
@@ -52,27 +52,27 @@ RenderObject3D& RenderObject3D::operator=(const RenderObject3D& other)
     return *this;
 }
 
-RenderObject3D::RenderObject3D(std::vector<GLfloat>* vertexData, std::vector<GLuint>* indexData) : RenderObject(vertexData, indexData), NBO(0)
+RenderObject3D::RenderObject3D(std::vector<GLfloat>* vertexData, std::vector<GLuint>* indexData) : RenderObject(vertexData, indexData), NBO{}
 {
     glGenBuffers(1, &NBO);
 }
 
-RenderObject3D::RenderObject3D(std::string ObjectName) : RenderObject(), NBO(0)
+RenderObject3D::RenderObject3D(std::string ObjectName) : RenderObject(), NBO{}
 {
     glGenBuffers(1, &NBO);
     ObjectName = "models/" + ObjectName;
-    vector<GLuint> vertexIndices, UVIndices, normalIndices;
-    vector<vec3> rawVertices;
-    vector<vec2> rawUVs;
-    vector<vec3> rawNormals;
-    vector<vec3> vertices;
-    vector<vec2> UVs;
-    vector<vec3> normals;
-    ifstream file(ObjectName);
-    string word;
-    vec3 vertex;
-    vec2 coord;
-    GLuint index;
+    vector<GLuint> vertexIndices{}, UVIndices{}, normalIndices{};
+    vector<vec3> rawVertices{};
+    vector<vec2> rawUVs{};
+    vector<vec3> rawNormals{};
+    vector<vec3> vertices{};
+    vector<vec2> UVs{};
+    vector<vec3> normals{};
+    ifstream file{ObjectName};
+    string word{};
+    vec3 vertex{};
+    vec2 coord{};
+    GLuint index{};
     if(!file.is_open())
     {
         ERROR << "Failed to open file: " << ObjectName << endl;
