9ef3b5c3315b5d888dd18eca14247cb87b263985
GenaBitu <genabitu@gmail.com>
2014-10-14 10:49:06 +0000

    Unresolded pointer-to-member-function

--- src/Slider.cpp
+++ src/Slider.cpp
@@ -1,3 +1,7 @@
 #include "../Main.hpp"
 using namespace std;
 using namespace glm;
+
+Slider::Slider(glm::vec2 inPosition, glm::vec2 inSize, std::string texBG, int maxState, glm::vec2 buttonSize, std::string texLeftPressed, std::string texLeftUnpressed) : Image(inPosition, inSize, texBG), state{}, maxState{maxState}, leftButton{new Button(inPosition, buttonSize, texLeftPressed, texLeftUnpressed, this->decrease1)}
+{
+}
