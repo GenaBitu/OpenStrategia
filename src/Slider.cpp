153ea82a5654e2b8711f4ce1f8ccf5c205e2238f
GenaBitu <genabitu@gmail.com>
2014-10-26 14:20:34 +0100

    Slider 1st implementation

--- src/Slider.cpp
+++ src/Slider.cpp
@@ -3,18 +3,36 @@ using namespace std;
 using namespace glm;
 
 Slider::Slider(glm::vec2 inPosition, glm::vec2 inSize, std::string texBG, int maxState, glm::vec2 buttonSize, std::string texLeftPressed, std::string texLeftUnpressed) : Image(inPosition, inSize, texBG), state{}, maxState{maxState},
-leftButton{new Button<Slider>(inPosition, buttonSize, texLeftPressed, texLeftUnpressed, &Slider::decrease, this)}
+leftButton{new Button<Slider>(inPosition, buttonSize, texLeftPressed, texLeftUnpressed, &Slider::decrease, this)},
+rightButton{new Button<Slider>(inPosition + inSize - buttonSize, buttonSize, texLeftPressed, texLeftUnpressed, &Slider::increase, this)}
 {
 }
 
 void Slider::handle()
 {
+    leftButton->handle();
+    rightButton->handle();
 }
 
 void Slider::render(std::shared_ptr<Program> prg) const
 {
+    Image::render(prg, 0);
+    leftButton->render(prg);
+    rightButton->render(prg);
 }
 
 void Slider::decrease()
 {
+    if(state > 0)
+    {
+        state--;
+    }
+}
+
+void Slider::increase()
+{
+    if(state < maxState)
+    {
+        state++;
+    }
 }
