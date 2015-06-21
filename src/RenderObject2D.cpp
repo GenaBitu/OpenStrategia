8a96df5c2ee5964405867b1ed22356ad326dcc45
GenaBitu <genabitu@gmail.com>
2015-06-21 14:34:43 +0200

    Using VAOs

--- src/RenderObject2D.cpp
+++ src/RenderObject2D.cpp
@@ -2,7 +2,17 @@
 using namespace std;
 using namespace glm;
 
-RenderObject2D::RenderObject2D() : RenderObject() {}
+RenderObject2D::RenderObject2D() : RenderObject()
+{
+    glBindVertexArray(VAO);
+
+    // Set correct size for VBO vertices (2)
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+    glBindVertexArray(0);
+}
 
 RenderObject2D::RenderObject2D(std::shared_ptr<std::vector<GLfloat>> vertexData, std::shared_ptr<std::vector<GLuint>> indexData) : RenderObject(vertexData, indexData) {}
 
@@ -10,8 +20,9 @@ void RenderObject2D::render(std::shared_ptr<Program> const prg, const GLint texU
 {
     glDisable(GL_DEPTH_TEST);
 
-    // Select shader program
+    // Select shader program and VAO
     glUseProgram(prg->ID);
+    glBindVertexArray(VAO);
 
     // Change coordinate system
     mat4 matrix = mat4{ 1, 0, 0, 0,
@@ -25,6 +36,9 @@ void RenderObject2D::render(std::shared_ptr<Program> const prg, const GLint texU
     GLint loc {glGetUniformLocation(prg->ID, "matrix")};
     glUniformMatrix4fv(loc, 1, GL_FALSE, value_ptr(matrix));
 
-    RenderObject::render(prg, 2, texUnit);
+    //Render the object
+    RenderObject::render(prg, texUnit);
+
+    glBindVertexArray(0);
     glEnable(GL_DEPTH_TEST);
 }
