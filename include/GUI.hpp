b27b53540fb4b476be6106faf971b1de2c7660e5
GenaBitu <genabitu@gmail.com>
2015-09-04 14:55:04 +0200

    GUI.hpp
    for including GUI headers

---
+++ include/GUI.hpp
@@ -0,0 +1,19 @@
+/**
+ * \file GUI.hpp
+ * \brief GUI header file intermediary
+ *
+ * Includes all header files for GUI classes
+ * \author GenaBitu
+ **/
+
+#ifndef GUI_HPP
+#define GUI_HPP
+
+#include "GUI/Button.hpp"
+#include "GUI/Checkbox.hpp"
+#include "GUI/Image.hpp"
+#include "GUI/MultiButton.hpp"
+#include "GUI/Slider.hpp"
+#include "GUI/Text.hpp"
+
+#endif // GUI_HPP
--- include/Main.hpp
+++ include/Main.hpp
@@ -2,7 +2,7 @@
  * \file Main.hpp
  * \brief Main header file
  *
- * Includes all other header files and declares all global-scope variables
+ * Includes all other header files
  * \author GenaBitu
  **/
 
@@ -11,18 +11,14 @@
 
 #include "Global.hpp"
 
-#include "GUI/Button.hpp"
+#include "GUI.hpp"
+
 #include "Camera.hpp"
-#include "GUI/Checkbox.hpp"
 #include "Font.hpp"
-#include "GUI/Image.hpp"
-#include "GUI/MultiButton.hpp"
 #include "Program.hpp"
 #include "RenderObject.hpp"
 #include "RenderObject2D.hpp"
 #include "RenderObject3D.hpp"
-#include "GUI/Slider.hpp"
-#include "GUI/Text.hpp"
 #include "Texture.hpp"
 
 #endif // MAIN_HPP
